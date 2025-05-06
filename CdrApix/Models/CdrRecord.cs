970b62b57c5d36c0e58fc8788cd86cdb70c7dc47
Asad Kayani <122390889+asadkayani3240@users.noreply.github.com>
2025-05-06 17:34:13 +0500

    Add .gitignore and initial commit

---
+++ CdrApix/Models/CdrRecord.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CdrApix.Models
+{
+    [Table("CdrRecords")]
+    public class CdrRecord
+    {
+        [Key]
+        public string Reference { get; set; }
+        public string CallerId { get; set; }
+        public string Recipient { get; set; }
+        public DateTime CallDate { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public int Duration { get; set; }
+        [Column(TypeName = "decimal(18,3)")]
+        public decimal Cost { get; set; }
+        public string Currency { get; set; }
+    }
+}
